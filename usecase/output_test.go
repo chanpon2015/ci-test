25b4afe2e0f752206fdb04fdec8003d573ab8191
chanpon2015 <40635072+chanpon2015@users.noreply.github.com>
2020-11-10 12:30:43 +0900

    Create output_test.go

---
+++ usecase/output_test.go
@@ -0,0 +1,15 @@
+package usecase_test
+
+import (
+	"testing"
+
+	"github.com/chanpon2015/test/usecase"
+)
+
+func TestOut(t *testing.T) {
+	o := usecase.NewOutput()
+	if err := o.Out(); err != nil {
+		t.Fatal(err)
+	}
+	t.Log("success")
+}
